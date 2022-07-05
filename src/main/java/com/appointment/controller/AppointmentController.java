f20547f80f6cd011655618806ccda323f058054a
MikeQin <mike.qin@gmail.com>
2022-07-04 22:57:43 -0500

    initial

---
+++ src/main/java/com/appointment/controller/AppointmentController.java
@@ -0,0 +1,10 @@
+package com.appointment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/appointments")
+public class AppointmentController {
+
+}
